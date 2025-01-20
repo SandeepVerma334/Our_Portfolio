9cc932d5100458aec1a2d8c68543545cf95d13fa
suman verma <sumanverma26102004@gmail.com>
2025-01-20 13:03:32 +0530

    done

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "our_portfolio"; // Ensure this matches your database
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn){
+  echo"success";
+}
+else{
+    echo"error";
+}
+?>
